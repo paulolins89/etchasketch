ee1ab672235afca6486e05500b0ce2b60407bc17
Paulo Lins <paulolins89@gmail.com>
2021-03-16 13:49:41 -0300

    was able to change the grid size, now need to fix hovering deal

--- etchasketch.js
+++ etchasketch.js
@@ -1,16 +1,21 @@
+//This sets the initial grid at 16x16
 const container = document.querySelector('.container');
-
 let size = 16;
-let gridSize = size * size;
-container.style.gridTemplateColumns = "repeat("+size+", auto)"
-container.style.gridTemplateRolumns = "repeat("+size+", auto)"
-for(let i = 1; i <= gridSize; i++){
-    const div = document.createElement('div');
-    div.classList.add('box');
-    container.appendChild(div)
+makeGrid(size);
+
+function makeGrid(size){
+    let gridSize = size * size;
+    container.style.gridTemplateColumns = "repeat("+size+", auto)"
+    container.style.gridTemplateRolumns = "repeat("+size+", auto)"
+    for(let i = 1; i <= gridSize; i++){
+        const div = document.createElement('div');
+        div.classList.add('box');
+        container.appendChild(div)
+    }
 }
 
 
+//sets an eventlistener for hovering
 const boxes = document.querySelectorAll('.box');
 boxes.forEach((div) => {
     div.addEventListener('mouseover', () => {
@@ -19,38 +24,27 @@ boxes.forEach((div) => {
 });
 
 const clearButton = document.querySelector("#clearButton");
-clearButton.onclick = ( () => {
-    size = prompt('Select new size (max 100)');
-    //check to see if number is string or number itself
-    while (size > 100){
-        size = prompt('Invalid size. Select another (max 100)');
-    }
+clearButton.addEventListener('click', () => {
+    makeNewGrid();
 });
-
-function clearBoxBackground (){
-    boxes.forEach((div) => {
-        div.classList.remove('hovered');
-    })
-}
 
 function makeNewGrid (){
     let newSize = prompt('Select new size (min 2, max 100)');
     while ((newSize > 100) || (newSize < 2)){
         newSize = prompt('Invalid size, Select another (min 2, max 100)')
     }
-
+    deleteOldGrid();
+    makeGrid(newSize);
 }
 
 function deleteOldGrid(){
-    while(container.hadChildNodes()){
-        container.removeChild(div);
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild);
     }
 }
 
-function defineSize(size){
-    if(arguments[0] == undefined){
-        return 16;
-    }else{
-        return size;
-    }
+function clearBoxBackground (){
+    boxes.forEach((div) => {
+        div.classList.remove('hovered');
+    })
 }
