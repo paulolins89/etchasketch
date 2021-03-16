e7b4af711e1e57b768159538da1901d8b2c3ed01
Paulo Lins <paulolins89@gmail.com>
2021-03-16 13:30:34 -0300

    added some more ideas to clear the page

--- etchasketch.js
+++ etchasketch.js
@@ -1,16 +1,15 @@
 const container = document.querySelector('.container');
 
-try{ 
-    let gridSize = defineSize(size)*defineSize(size);
-}catch(err){
-    let gridSize = 16*16;
-}
-
+let size = 16;
+let gridSize = size * size;
+container.style.gridTemplateColumns = "repeat("+size+", auto)"
+container.style.gridTemplateRolumns = "repeat("+size+", auto)"
 for(let i = 1; i <= gridSize; i++){
     const div = document.createElement('div');
     div.classList.add('box');
     container.appendChild(div)
 }
+
 
 const boxes = document.querySelectorAll('.box');
 boxes.forEach((div) => {
@@ -19,22 +18,39 @@ boxes.forEach((div) => {
     });
 });
 
-const clearButton = document.querySelector('#clrBtn');
-clearButton.onclick = () => {
+const clearButton = document.querySelector("#clearButton");
+clearButton.onclick = ( () => {
     size = prompt('Select new size (max 100)');
     //check to see if number is string or number itself
     while (size > 100){
         size = prompt('Invalid size. Select another (max 100)');
     }
-    defineSize(size);
+});
+
+function clearBoxBackground (){
+    boxes.forEach((div) => {
+        div.classList.remove('hovered');
+    })
 }
 
-function defineSize (size) {
-    let newSize = 0;
-    if (size === undefined){
-        newSize = 16;
-    }else{
-        newSize = size;
+function makeNewGrid (){
+    let newSize = prompt('Select new size (min 2, max 100)');
+    while ((newSize > 100) || (newSize < 2)){
+        newSize = prompt('Invalid size, Select another (min 2, max 100)')
     }
-    return newSize
+
+}
+
+function deleteOldGrid(){
+    while(container.hadChildNodes()){
+        container.removeChild(div);
+    }
+}
+
+function defineSize(size){
+    if(arguments[0] == undefined){
+        return 16;
+    }else{
+        return size;
+    }
 }
